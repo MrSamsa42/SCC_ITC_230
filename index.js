1f01d02bbf4b685ad0119bb8cca6d247b765a94b
Jonathan Adler <jradler@gmail.com>
2018-06-29 01:07:19 +0000

    Home page now renders home.html

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var http = require("http");
+var fs = require("fs");
 
 http.createServer(function(req, res){
     var path = req.url.toLowerCase();
     switch(path) {
-        case '/' :
-            res.writeHead('200', {'Content-Type' : 'text/plain'});
-            res.end('This is the home page');
+        case '/':
+            fs.readFile('public/home.html', function (err, data) {
+                if (err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            });
             break;
         case '/about' :
             res.writeHead('200', {'Content-Type' : 'text/plain'});
