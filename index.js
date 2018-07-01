bbc25960c04bf67c4efa16cf523adfa45fef7b1e
Jonathan Adler <jradler@gmail.com>
2018-07-01 20:40:38 +0000

    Refactored index.js using ES6 syntax

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-var http = require("http");
-var fs = require("fs");
+const http = require("http");
+const fs = require("fs");
 
-http.createServer(function(req, res){
-    var path = req.url.toLowerCase();
+http.createServer( (req, res) => {
+    let path = req.url.toLowerCase();
     switch(path) {
         case '/':
-            fs.readFile('public/home.html', function (err, data) {
+            fs.readFile('public/home.html', (err, data) => {
                 if (err) return console.error(err);
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(data.toString());
             });
             break;
         case '/about' :
-            fs.readFile('public/about.html', function(err, data){
+            fs.readFile('public/about.html', (err, data) => {
                 if(err) return console.error(err);
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(data.toString());
             })
             break;
         default :
-            fs.readFile('public/404.html', function(err, data){
+            fs.readFile('public/404.html', (err, data) =>{
                 if(err) return console.error(err);
                 res.writeHead(200, {'Content-Type': 'text/html'});
                 res.end(data.toString());
             })
             break;
     }
-}).listen(process.env.PORT || 3000, function(){
+}).listen(process.env.PORT || 3000, () => {
     console.log("The server is running!");
 });
