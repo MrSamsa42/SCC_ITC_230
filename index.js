263baabd9f79599174903c33d901800ff5286a37
Jonathan Adler <jradler@gmail.com>
2018-07-01 20:31:39 +0000

    Added route to 404 page with message.  All HW1 functionality now in place.

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ http.createServer(function(req, res){
             })
             break;
         default :
-            res.writeHead('404', {'Content-Type' : 'text/plain'});
-            res.end('Page not found');
+            fs.readFile('public/404.html', function(err, data){
+                if(err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            })
             break;
     }
 }).listen(process.env.PORT || 3000, function(){
