01c216245aec32b143ef58abbecd4c1cac71077b
Jonathan Adler <jradler@gmail.com>
2018-07-01 20:19:46 +0000

    Added route to render the About page.  Added Semantic-UI CDN.

--- index.js
+++ index.js
@@ -12,8 +12,11 @@ http.createServer(function(req, res){
             });
             break;
         case '/about' :
-            res.writeHead('200', {'Content-Type' : 'text/plain'});
-            res.end('This is the about page');
+            fs.readFile('public/about.html', function(err, data){
+                if(err) return console.error(err);
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data.toString());
+            })
             break;
         default :
             res.writeHead('404', {'Content-Type' : 'text/plain'});
