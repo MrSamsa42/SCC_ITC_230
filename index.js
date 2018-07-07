e8899fd735fe4b250db6236e82e0a1144bc0ca36
Jonathan Adler <jradler@gmail.com>
2018-07-07 21:22:37 +0000

    movies module in place; working /get route.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const http = require("http");
 const fs = require("fs");
+const movies = require("./lib/movies");
+const url = require('url');
 
 http.createServer( (req, res) => {
-    let path = req.url.toLowerCase();
+    //const routeURL = url.parse(req.url);
+    let base = "https://7b4b165d23a942ed9a7b8c118ebed6e5.vfs.cloud9.us-west-2.amazonaws.com";
+    const routeURL = new url.URL(req.url, base);
+    //let path = routeURL.pathname.toLocaleLowerCase();
+    let path = routeURL.pathname;
+    console.log(path);
+    let params = new url.URLSearchParams(routeURL.search);
+    console.log(params.get('title'));
     switch(path) {
         case '/':
             fs.readFile('public/home.html', (err, data) => {
@@ -18,6 +27,15 @@ http.createServer( (req, res) => {
                 res.end(data.toString());
             })
             break;
+        case '/get' :
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let title = params.get('title');
+            //res.end(`Searching for ${title} ... \n`);
+            let movie = movies.getMovie(title);
+            let movieString = (movie) ? JSON.stringify(movie) : `${title} was not found!`;
+            res.end(movieString);
+            break;
+            
         default :
             fs.readFile('public/404.html', (err, data) =>{
                 if(err) return console.error(err);
@@ -29,3 +47,8 @@ http.createServer( (req, res) => {
 }).listen(process.env.PORT || 3000, () => {
     console.log("The server is running!");
 });
+
+
+//console.log(movies.getMovie("Scouts Guide to the Zombie Apocalypse"));
+//console.log(movies.deleteMovie("Scouts Guide to the Zombie Apocalypse"));
+//console.log(movies.getMovie("Scouts Guide to the Zombie Apocalypse"));
