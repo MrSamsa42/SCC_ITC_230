710452341c5b26c384e366fa79be16484ad2784d
Jonathan Adler <jradler@gmail.com>
2018-07-09 16:14:47 +0000

    Shortened and simplified the movieList, added route to add new movie

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ const movies = require("./lib/movies");
 const url = require('url');
 
 http.createServer( (req, res) => {
-    //const routeURL = url.parse(req.url);
     let base = "https://7b4b165d23a942ed9a7b8c118ebed6e5.vfs.cloud9.us-west-2.amazonaws.com";
-    const routeURL = new url.URL(req.url, base);
-    //let path = routeURL.pathname.toLocaleLowerCase();
-    let path = routeURL.pathname;
-    console.log(path);
-    let params = new url.URLSearchParams(routeURL.search);
-    console.log(params.get('title'));
+    let completeURL = new url.URL(req.url, base);
+    let path = completeURL.pathname;
+    let params = new url.URLSearchParams(completeURL.search);
     let title = {};
     let intro = "";
     switch(path) {
@@ -33,17 +29,27 @@ http.createServer( (req, res) => {
             res.writeHead(200, {'Content-Type': 'text/plain'});
             title = params.get('title');
             intro = `Searching for ${title}... \n`;
-            let deleteMovieString = `${intro} ${movies.deleteMovie(title)}`;
+            let deletedMovie = movies.deleteMovie(title);
+            let deleteMovieString = (deletedMovie) ? `${intro} ${title} was removed` : `${intro}${title} was not found!`;
             res.end(deleteMovieString);
-            
             break;
         case '/get' :
             res.writeHead(200, {'Content-Type': 'text/plain'});
             title = params.get('title');
             intro = `Searching for ${title}... \n`;
-            let movieString = (movies.getMovie(title)) ? `${intro} ${JSON.stringify(movies.getMovie(title))}` : `${intro}${title} was not found!`;
+            let movieString = (movies.getMovie(title)) ? `${intro} ${JSON.stringify(movies.getMovie(title))}`:`${intro}${title} was not found!`;
             res.end(movieString);
             break;
+        case '/add' :
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            let newMovie = {};
+            newMovie.title = params.get('title');
+            newMovie.year = params.get('year');
+            newMovie.imdbID = params.get('imdbID');
+            newMovie.poster = params.get('poster');
+            let added = movies.addMovie(newMovie);
+            let addedMovieString = (added) ? `${newMovie.title} was added!` : `${newMovie.title} already exists!`;
+            res.end(addedMovieString);
         default :
             fs.readFile('public/404.html', (err, data) =>{
                 if(err) return console.error(err);
@@ -56,7 +62,3 @@ http.createServer( (req, res) => {
     console.log("The server is running!");
 });
 
-
-//console.log(movies.getMovie("Scouts Guide to the Zombie Apocalypse"));
-//console.log(movies.deleteMovie("Scouts Guide to the Zombie Apocalypse"));
-//console.log(movies.getMovie("Scouts Guide to the Zombie Apocalypse"));
